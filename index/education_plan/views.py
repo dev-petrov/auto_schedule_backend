07cfda1543404e7c225737aaa6253b0fe10d9259
Anton250 <antosha250900@gmail.com>
2021-06-14 19:17:48 +0300

    added possibility to upload file

--- index/education_plan/views.py
+++ index/education_plan/views.py
@@ -1,5 +1,7 @@
-from index.models import EducationPlan
+import pandas as pd
+from index.models import Discipline, EducationPlan, Group
 from rest_framework import serializers, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from django_filters.rest_framework import FilterSet, CharFilter
 from index.lesson.views import SpecDisciplineSerializer, SpecGroupSerializer
@@ -14,6 +16,18 @@ class EducationPlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = EducationPlan
         fields = '__all__'
+
+class EducationPlanUploadSerializer(serializers.Serializer):
+    TYPE_ALL = 'A'
+    TYPE_GROUP = 'G'
+    group_id = serializers.IntegerField(required=False, allow_null=True)
+    type = serializers.ChoiceField(choices=[TYPE_ALL, TYPE_GROUP])
+    file = serializers.FileField()
+
+    def is_valid(self, raise_exception):
+        super().is_valid(raise_exception=raise_exception)
+        if (self.validated_data['type'] == self.TYPE_GROUP and not self.validated_data.get('group_id')):
+            raise serializers.ValidationError('Нужно выбрать группу для типа загрузки через группу.')
 
 
 class EducationPlanFilter(FilterSet):
@@ -32,3 +46,57 @@ class EducationPlanViewSet(viewsets.ModelViewSet):
     queryset = EducationPlan.objects.all()
     serializer_class = EducationPlanSerializer
     filterset_class = EducationPlanFilter
+
+    @action(detail=False, methods=['post',])
+    def upload(self, request):
+        data = EducationPlanUploadSerializer(data=request.data)
+        data.is_valid(True)
+        try:
+            df = pd.read_excel(data.validated_data['file'].file.read(), engine='openpyxl')
+        except:
+            raise serializers.ValidationError('Неправильный формат файла')
+        type = data.validated_data['type']
+        disciplines = {
+            d.title: d
+            for d in Discipline.objects.all()
+        }
+        plans = []
+        if type == EducationPlanUploadSerializer.TYPE_ALL:
+            COL_GROUP = df.columns[0]
+            COL_DISCIPLINE = df.columns[1]
+            COL_COUNT = df.columns[2]
+            groups = {
+                g.code: g
+                for g in Group.objects.all()
+            }
+            for _, row in df.iterrows():
+                group = groups.get(row[COL_GROUP])
+                discipline = disciplines.get(row[COL_DISCIPLINE])
+                if not group:
+                    raise serializers.ValidationError(f'Группы с кодом {row[COL_GROUP]} не сущестует')
+                if not discipline:
+                    raise serializers.ValidationError(f'Дисциплины {row[COL_DISCIPLINE]} не сущестует')
+                plans.append(EducationPlan(
+                    group=group,
+                    discipline=discipline,
+                    lessons_in_week=row[COL_COUNT],
+                ))
+            EducationPlan.objects.filter(group__code__in=df[COL_GROUP].unique()).delete()
+        else:
+            COL_DISCIPLINE = df.columns[0]
+            COL_COUNT = df.columns[1]
+            group_id = data.validated_data['group_id']
+            for _, row in df.iterrows():
+                discipline = disciplines.get(row[COL_DISCIPLINE])
+                if not discipline:
+                    raise serializers.ValidationError(f'Дисциплины {row[COL_DISCIPLINE]} не сущестует')
+                plans.append(EducationPlan(
+                    group_id=group_id,
+                    discipline=discipline,
+                    lessons_in_week=row[COL_COUNT],
+                ))
+            EducationPlan.objects.filter(group_id=group_id).delete()
+        
+        EducationPlan.objects.bulk_create(plans)
+
+        return Response()
